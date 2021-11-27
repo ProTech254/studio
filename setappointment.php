b26764b4db9a47cdfe92a66258e7e74f132ddbef
prf-alekie <gichualex47@gmail.com>
2021-11-27 23:01:45 +0300

    update

--- setappointment.php
+++ setappointment.php
@@ -1,22 +1,22 @@
 <?php
 	function set_appointment(){
 		extract($_POST);
-		$doc = $this->db->query("SELECT * FROM designers_list where id = ".$doctor_id);
+		$doc = $this->db->query("SELECT * FROM designers_list where id = ".$designer_id);
 		$schedule = date('Y-m-d',strtotime($date)).' '.date('H:i',strtotime($time)).":00";
 		$day = date('l',strtotime($date));
 		$time = date('H:i',strtotime($time)).":00";
 		$sched = date('H:i',strtotime($time));
-		$doc_sched_check = $this->db->query("SELECT * FROM designers_schedule where doctor_id = $doctor_id and day = '$day' and ('$time' BETWEEN time_from and time_to )");
+		$doc_sched_check = $this->db->query("SELECT * FROM designers_schedule where designer_id = $designer_id and day = '$day' and ('$time' BETWEEN time_from and time_to )");
 		if($doc_sched_check->num_rows <= 0){
-			return json_encode(array('status'=>2,"msg"=>"Appointment schedule not valid for selected doctor's schedule."));
+			return json_encode(array('status'=>2,"msg"=>"Appointment schedule not valid for selected designer's schedule."));
 			exit;
 		}
 
-		$data = " doctor_id = '$doctor_id' ";
-		if(!isset($patient_id))
-		$data .= ", patient_id = '".$_SESSION['login_id']."' ";
+		$data = " designer_id = '$designer_id' ";
+		if(!isset($user_id))
+		$data .= ", user_id = '".$_SESSION['login_id']."' ";
 		else
-		$data .= ", p_id = '$patient_id' ";
+		$data .= ", p_id = '$user_id' ";
 
 		$data .= ", schedule = '$schedule' ";
 		if(isset($status))
